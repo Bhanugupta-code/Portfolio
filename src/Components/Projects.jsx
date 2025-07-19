999e3a925acfb74778cf8a552e575b291f20d8dc
Bhanu-coding <guptabhanu782@gmail.com>
2025-07-19 11:12:40 +0530

    After correction

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,6 +3,7 @@ import ProjectCards from './ProjectCards';
 import todo from '../assets/todo.png'
 import Task from '../assets/Task Manager.png'
 import clickNbuy from '../assets/clickNbuy.png'
+import web from '../assets/image (1).png'
 import designer from '../assets/designer studio.png'
 const Projects = () => {
   return (
@@ -10,10 +11,11 @@ const Projects = () => {
         <h1 className='text-2xl md:text-4xl text-white font-bold'>Projects</h1>
        
         <div className="sm:py-12 sm:px-12 lg:px-24 p-2 grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2  gap-x-10">
+        <ProjectCards title="E-commerce web" code="https://github.com/Bhanugupta-code/QTNReactAssignment" img={web} main="This app is made in react with Tailwind." demo="https://qtn-react-assignment.vercel.app/" />
         <ProjectCards title="Todo App" code="https://github.com/Bhanugupta-code/Todo-app" img={todo} main="This app is made in react with local storage." demo="https://todo-six-gules.vercel.app/" />
+        <ProjectCards title="Designer Studio" img={designer} demo="https://designer-studio-five.vercel.app/" code="https://github.com/Bhanugupta-code/Designer-Studio"  main="This is the cloned of the mulberry art website. This website is made in react ."/>
         <ProjectCards title="Click N Buy" code="https://github.com/Bhanugupta-code/clickNbuy" img={clickNbuy} main="This app is made in react with bootstrap." demo="https://click-nbuy.vercel.app/" />
         <ProjectCards title="Task Manager" img={Task} demo="https://task-manager-liart-beta.vercel.app/" code="https://github.com/Bhanugupta-code/Task-Manager"  main="This is the Enhanced version of Todo app with additional functionality moving task and give priority to task."/>
-        <ProjectCards title="Designer Studio" img={designer} demo="https://designer-studio-five.vercel.app/" code="https://github.com/Bhanugupta-code/Designer-Studio"  main="This is the cloned of the mulberry art website. This website is made in react ."/>
         </div>
       
     </div>
